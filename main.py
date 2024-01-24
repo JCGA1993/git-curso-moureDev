08f0e4f096e1426c3a4fe0bbfe9fbdaba485b956
JCGA1993 <kamygz93@gmail.com>
2024-01-24 20:44:35 +0200

    manipule el arcgivo de main desde main

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
 print("nuevas caracteristicas en la rama principal")
 print("estoy haciendo algunos avances en la rama main luego de haber creado la rama login con git")
+print(" este archivo no lo tocan los de login")
